88df523084d630f438cb5fd4120a2554fa32e417
badgersky <szymonek.ptasznik@gmail.com>
2025-01-08 00:31:58 +0100

    changed specification of register semaphores

--- sem.c
+++ sem.c
@@ -1,33 +1,46 @@
 #include "sem.h"
 
-sem_t *door, *reg, *doctors[6];
+sem_t *door, *reg[2], *doctors[6];
 
 void initialize_sem() {
     for (int i = 0; i < 6; i++) {
         doctors[i] = (sem_t*) mmap(0, sizeof(sem_t), PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
         if (doctors[i] == MAP_FAILED) {perror("mmap"); exit(2);}
     }
-
+    for (int i = 0; i < 2; i++) {
+        reg[i] = (sem_t*) mmap(0, sizeof(sem_t), PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
+        if (reg == MAP_FAILED) {perror("mmap"); exit(2);}
+    }
     door = (sem_t*) mmap(0, sizeof(sem_t), PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
     if (door == MAP_FAILED) {perror("mmap"); exit(2);}
-    reg = (sem_t*) mmap(0, sizeof(sem_t), PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
-    if (reg == MAP_FAILED) {perror("mmap"); exit(2);}
 
     for (int i = 0; i < 6; i++) {
         if (sem_init(doctors[i], 1, 1) < 0) {perror("sem_init"); exit(2);}
     }
+    for (int i = 0; i < 2; i++) {
+        if (sem_init(reg[i], 1, 2) < 0) {perror("sem_init"); exit(2);}
+    }
     if (sem_init(door, 1, 1) < 0) {perror("sem_init"); exit(2);}
-    if (sem_init(reg, 1, 2) < 0) {perror("sem_init"); exit(2);}
+
 }
 
 void destroy_sem() {
     for (int i = 0; i < 6; i++) {
         if (sem_destroy(door) < 0) {perror("sem_destroy"); exit(2);}
     }
+    for (int i = 0; i < 2; i++) {
+        if (sem_destroy(reg[i]) < 0) {perror("sem_destroy"); exit(2);}
+    }
     if (sem_destroy(door) < 0) {perror("sem_destroy"); exit(2);}
-    if (sem_destroy(reg) < 0) {perror("sem_destroy"); exit(2);}
+
+    for (int i = 0; i < 6; i++) {
+        if (munmap(doctors[i], sizeof(sem_t)) < 0) {perror("munmap"); exit(2);}
+    }
+    for (int i = 0; i < 2; i++) {
+        if (munmap(reg[i], sizeof(sem_t)) < 0) {perror("munmap"); exit(2);}
+    }
     if (munmap(door, sizeof(sem_t)) < 0) {perror("munmap"); exit(2);}
-    if (munmap(reg, sizeof(sem_t)) < 0) {perror("munmap"); exit(2);}
+
 }
 
 void print_sem_val(sem_t* sem) {
--- sem.h
+++ sem.h
@@ -7,7 +7,7 @@
 #define SEM_H
 
 extern sem_t *door;
-extern sem_t *reg;
+extern sem_t *reg[2];
 extern sem_t* doctors[6];
 
 #endif
