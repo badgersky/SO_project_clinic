5853db764866d2d58d95f50475701ac7a4413ce6
szymon <szymon.pt4sznik@gmail.com>
2025-01-11 22:08:30 +0100

    creates only one registration process, number of desks is controled by semaphore

--- register.c
+++ register.c
@@ -1,8 +1,8 @@
 #include "register.h"
 
-void register_routine(int i) {
+void register_routine() {
     do {
-        printf("register %d\n", i);
+        printf("register\n");
         sleep(1);
     } while(1);
 }
@@ -10,23 +10,19 @@ void register_routine(int i) {
 void create_registers() {
     pid_t pid;
 
-    for (int i = 1; i <= REG_NUM; i++) {
-        pid = fork();
-        if (pid < 0) {
-            return perror("fork");
-            exit(1);
-        }
-        if (pid == 0) {
-            register_routine(i);
-        }
+    pid = fork();
+    if (pid < 0) {
+        return perror("fork");
+        exit(1);
+    }
+    if (pid == 0) {
+        register_routine();
     }
 }
 
-void wait_registers() {
-    for (int i = 1; i <= REG_NUM; i++) {
-        if (wait(0) < 0) {
-            perror("wait");
-            exit(1);
-        }
+void wait_registers() { 
+    if (wait(0) < 0) {
+        perror("wait");
+        exit(1);
     }
 }
